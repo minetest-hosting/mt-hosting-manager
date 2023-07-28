9e570b1799f4cf5ecea79dfed525d51ba893e1f6
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-07-28 14:53:13 +0200

    node type info

--- web/usernode/list.go
+++ web/usernode/list.go
@@ -1,9 +1,15 @@
 package usernode
 
 import (
+	"fmt"
 	"mt-hosting-manager/types"
 	"net/http"
 )
+
+type UserNodeInfo struct {
+	*types.UserNode
+	*types.NodeType
+}
 
 // show all nodes by the user
 func (ctx *Context) List(w http.ResponseWriter, r *http.Request, c *types.Claims) {
@@ -13,8 +19,27 @@ func (ctx *Context) List(w http.ResponseWriter, r *http.Request, c *types.Claims
 		return
 	}
 
-	model := make(map[string]any)
-	model["Nodes"] = nodes
+	nodeinfos := make([]*UserNodeInfo, len(nodes))
+	for i, n := range nodes {
+		nt, err := ctx.repos.NodeTypeRepo.GetByID(n.NodeTypeID)
+		if err != nil {
+			ctx.tu.RenderError(w, r, 500, err)
+			return
+		}
+		if nt == nil {
+			ctx.tu.RenderError(w, r, 404, fmt.Errorf("nodetype not found: '%s'", n.NodeTypeID))
+			return
+		}
+
+		nodeinfos[i] = &UserNodeInfo{
+			UserNode: n,
+			NodeType: nt,
+		}
+	}
+
+	model := map[string]any{
+		"Nodes": nodeinfos,
+	}
 
 	ctx.tu.ExecuteTemplate(w, r, "usernode/list.html", model)
 }
