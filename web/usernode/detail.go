2f7722e6f7c0b5cda6fd7ac2e62dda6504b2a539
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-03 11:28:50 +0200

    ux

--- web/usernode/detail.go
+++ web/usernode/detail.go
@@ -8,8 +8,18 @@ import (
 )
 
 type DetailModel struct {
-	UserNode  *types.UserNode
-	LatestJob *types.Job
+	UserNode      *types.UserNode
+	LatestJob     *types.Job
+	DiskPercent   int
+	DiskGBUsed    float64
+	DiskGBTotal   float64
+	DiskWarn      bool
+	DiskDanger    bool
+	MemoryPercent int
+	MemoryGBUsed  float64
+	MemoryGBTotal float64
+	MemoryWarn    bool
+	MemoryDanger  bool
 }
 
 // view details
@@ -29,9 +39,28 @@ func (ctx *Context) Detail(w http.ResponseWriter, r *http.Request, c *types.Clai
 		return
 	}
 
+	bytes_in_gb := 1024.0 * 1024.0 * 1024.0
 	m := &DetailModel{
-		UserNode:  node,
-		LatestJob: job,
+		UserNode:      node,
+		LatestJob:     job,
+		DiskPercent:   int(float64(node.DiskUsed) / float64(node.DiskSize) * 100),
+		DiskGBUsed:    float64(node.DiskUsed) / bytes_in_gb,
+		DiskGBTotal:   float64(node.DiskSize) / bytes_in_gb,
+		MemoryPercent: int(float64(node.MemoryUsed) / float64(node.MemorySize) * 100),
+		MemoryGBUsed:  float64(node.MemoryUsed) / bytes_in_gb,
+		MemoryGBTotal: float64(node.MemorySize) / bytes_in_gb,
+	}
+
+	if m.DiskPercent > 90 {
+		m.DiskDanger = true
+	} else if m.DiskPercent > 75 {
+		m.DiskWarn = true
+	}
+
+	if m.MemoryPercent > 90 {
+		m.MemoryDanger = true
+	} else if m.MemoryPercent > 75 {
+		m.MemoryWarn = true
 	}
 
 	ctx.tu.ExecuteTemplate(w, r, "usernode/detail.html", m)
