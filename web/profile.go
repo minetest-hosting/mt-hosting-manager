9da8c140ab0825f1527c8eb7dcf00135a534ccc8
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-03 13:01:22 +0200

    profile page / usernode list fix

--- web/profile.go
+++ web/profile.go
@@ -11,14 +11,15 @@ type ProfileModel struct {
 
 func (ctx *Context) Profile(w http.ResponseWriter, r *http.Request, c *types.Claims) {
 
-	pm := &ProfileModel{}
-
 	user, err := ctx.repos.UserRepo.GetByMail(c.Mail)
 	if err != nil {
 		ctx.tu.RenderError(w, r, 500, err)
 		return
 	}
-	pm.User = user
 
-	ctx.tu.ExecuteTemplate(w, r, "profile.html", pm)
+	m := &ProfileModel{
+		User: user,
+	}
+
+	ctx.tu.ExecuteTemplate(w, r, "profile.html", m)
 }
