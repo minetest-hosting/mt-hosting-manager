4a0ae12aa51c0448202abe76fec2bde1ab4a79c3
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-17 15:20:17 +0200

    initialyze empty `minetest.conf`

--- worker/server_setup/setup.sh
+++ worker/server_setup/setup.sh
@@ -2,6 +2,11 @@
 set -e
 cd `dirname $0`
 
+# initialize minetest config if it does not exist
+test -f "minetest.conf" ||{
+    echo "server_name = {{.Servername}}" > "minetest.conf"
+}
+
 docker network create "network-{{.ServerShortID}}" || true
 docker-compose pull
 docker-compose up -d --remove-orphans
