ce848ec44c2f0bd427a876aa554094736ad4472e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-20 21:34:44 +0200

    vue rewrite

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,17 @@
+FROM node:20.5.1-alpine as node-app
+WORKDIR /public
+COPY /public/package-lock.json /public/package.json ./
+RUN npm ci
+COPY public/ .
+RUN npm run jshint && \
+	npm run bundle
+
 FROM golang:1.20.7 as go-app
 WORKDIR /data
 COPY go.* ./
 RUN go mod download
 COPY . .
+COPY --from=node-app /public /data/public
 RUN go test ./... -vet=off && \
 	go build -ldflags="-s -w -extldflags=-static"
 
---
+++ web/api.go
@@ -0,0 +1,45 @@
+package web
+
+import (
+	"mt-hosting-manager/db"
+	"mt-hosting-manager/public"
+	"mt-hosting-manager/web/middleware"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"github.com/vearutop/statigz"
+	"github.com/vearutop/statigz/brotli"
+)
+
+type Api struct {
+	repos *db.Repositories
+}
+
+func NewApi(repos *db.Repositories) *Api {
+	return &Api{
+		repos: repos,
+	}
+}
+
+func (api *Api) Setup() {
+	r := mux.NewRouter()
+	r.Use(middleware.LoggingMiddleware)
+	r.Use(middleware.PrometheusMiddleware)
+
+	// static files
+	if os.Getenv("WEBDEV") == "true" {
+		logrus.WithFields(logrus.Fields{"dir": "public"}).Info("Using live mode")
+		fs := http.FileServer(http.FS(os.DirFS("public")))
+		r.PathPrefix("/").HandlerFunc(fs.ServeHTTP)
+
+	} else {
+		logrus.Info("Using embed mode")
+		r.PathPrefix("/").Handler(statigz.FileServer(public.Webapp, brotli.AddEncoding))
+	}
+
+	// TODO: setup routes
+
+	http.Handle("/", r)
+}
