4f2501d27a320adc93e5c51fb37003a221a3dbe6
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-21 13:04:47 +0200

    oauth cleanup

--- web/api.go
+++ web/api.go
@@ -1,18 +1,14 @@
 package web
 
 import (
-	"fmt"
 	"mt-hosting-manager/db"
-	"mt-hosting-manager/notify"
 	"mt-hosting-manager/public"
 	"mt-hosting-manager/types"
 	"mt-hosting-manager/web/middleware"
 	"mt-hosting-manager/web/oauth"
 	"net/http"
 	"os"
-	"time"
 
-	"github.com/golang-jwt/jwt/v4"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 	"github.com/vearutop/statigz"
@@ -47,34 +43,17 @@ func (api *Api) Setup() {
 		r.PathPrefix("/").Handler(statigz.FileServer(public.Webapp, brotli.AddEncoding))
 	}
 
-	oauth_handler := &oauth.OauthHandler{
-		Impl:     &oauth.GithubOauth{},
-		UserRepo: api.repos.UserRepo,
-		Config:   api.cfg.GithubOauthConfig,
-		BaseURL:  api.cfg.BaseURL,
-		Type:     types.UserTypeGithub,
-		Callback: func(w http.ResponseWriter, user *types.User, new_user bool) error {
-			if new_user {
-				notify.Send(&notify.NtfyNotification{
-					Title:    fmt.Sprintf("New user signed up: %s", user.Name),
-					Message:  fmt.Sprintf("Name: %s, Mail: %s, Auth: %s", user.Name, user.Mail, user.Type),
-					Priority: 3,
-				}, true)
-			}
-			dur := time.Duration(24 * 180 * time.Hour)
-			claims := &types.Claims{
-				Mail:   user.Mail,
-				Role:   user.Role,
-				UserID: user.ID,
-				RegisteredClaims: &jwt.RegisteredClaims{
-					ExpiresAt: jwt.NewNumericDate(time.Now().Add(dur)),
-				},
-			}
-
-			return api.SetClaims(w, claims)
-		},
+	if api.cfg.GithubOauthConfig.ClientID != "" {
+		oauth_handler := &oauth.OauthHandler{
+			Impl:     &oauth.GithubOauth{},
+			UserRepo: api.repos.UserRepo,
+			Config:   api.cfg.GithubOauthConfig,
+			BaseURL:  api.cfg.BaseURL,
+			Type:     types.UserTypeGithub,
+			Callback: api.OauthCallback,
+		}
+		r.Handle("/oauth/callback", oauth_handler)
 	}
-	r.Handle("/oauth/callback", oauth_handler)
 
 	// TODO: setup routes
 
