396b8e760f0d35a8b950fce333a90931442663d8
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-22 15:15:29 +0200

    detail pages stub

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,8 +1,11 @@
 import Start from './components/pages/Start.js';
 import Profile from './components/pages/Profile.js';
 import NodeTypes from './components/pages/NodeTypes.js';
+import NodeTypeDetail from './components/pages/NodeTypeDetail.js';
 import MTServers from './components/pages/MTServers.js';
+import MTServerDetail from './components/pages/MTServerDetail.js';
 import UserNodes from './components/pages/UserNodes.js';
+import UserNodesDetail from './components/pages/UserNodesDetail.js';
 import Jobs from './components/pages/Jobs.js';
 
 export default [{
@@ -12,11 +15,18 @@ export default [{
 },{
 	path: "/nodes", component: UserNodes
 },{
+	path: "/nodes/:id", component: UserNodesDetail
+},{
 	path: "/mtservers", component: MTServers
+},{
+	path: "/mtservers/:id", component: MTServerDetail
 },{
 	path: "/jobs", component: Jobs,
 	meta: { requiredRole: "ADMIN" }
 },{
 	path: "/nodetypes", component: NodeTypes,
+	meta: { requiredRole: "ADMIN" }
+},{
+	path: "/nodetypes/:id", component: NodeTypeDetail,
 	meta: { requiredRole: "ADMIN" }
 }];
