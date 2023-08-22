d465b80816ccfc1fdbaf1b39c4d6ce6b4bececc5
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-22 15:25:45 +0200

    logout

--- public/js/components/pages/Login.js
+++ public/js/components/pages/Login.js
@@ -1,10 +1,14 @@
 import store from "../../store/info.js";
 import CardLayout from "../layouts/CardLayout.js";
+import { logout } from "../../api/login.js";
 
 export default {
 	data: () => store,
 	components: {
 		"card-layout": CardLayout
+	},
+	methods: {
+		logout: logout
 	},
 	template: /*html*/`
 	<card-layout>
@@ -14,6 +18,9 @@ export default {
 		<a :href="'https://github.com/login/oauth/authorize?client_id=' + github_client_id + '&scope=user:email'" class="btn btn-secondary">
 			<i class="fab fa-github"></i>
 			Login with Github
+		</a>
+		<a class="btn btn-primary" v-on:click="logout">
+			Logout
 		</a>
 	</card-layout>
     `
