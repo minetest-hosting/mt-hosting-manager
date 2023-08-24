c398ee712faf7d686bf4b7521ed27b34a6a9e5ff
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-24 20:15:36 +0200

    node detail

--- public/js/components/pages/UserNodesDetail.js
+++ public/js/components/pages/UserNodesDetail.js
@@ -1,7 +1,12 @@
 import CardLayout from "../layouts/CardLayout.js";
-import { get_by_id } from "../../api/node.js";
+import { get_by_id, update as update_node } from "../../api/node.js";
+import info_store from "../../store/info.js";
 
 const bytes_in_gb = 1000 * 1000 * 1000;
+
+function get_gb_rounded(bytes) {
+	return parseInt(bytes / bytes_in_gb * 100) / 100;
+}
 
 export default {
 	components: {
@@ -9,20 +14,39 @@ export default {
 	},
 	data: function() {
 		return {
+			info_store: info_store,
 			node: null,
-			disk_gb_total: 0
+			disk_gb_total: 0,
+			disk_gb_used: 0,
+			disk_percent: 0,
+			memory_gb_total: 0,
+			memory_gb_used: 0,
+			memory_percent: 0
 		};
 	},
 	mounted: function() {
 		this.update();
+		this.handle = setInterval(() => this.update(), 5000);
+	},
+	beforeUnmount: function() {
+		clearInterval(this.handle);
 	},
 	methods: {
 		update: function() {
 			get_by_id(this.$route.params.id)
 			.then(n => this.node = n)
 			.then(() => {
-				this.disk_gb_total = parseInt(this.node.disk_size / bytes_in_gb);
-			})
+				this.disk_gb_total = get_gb_rounded(this.node.disk_size);
+				this.disk_gb_used = get_gb_rounded(this.node.disk_used);
+				this.disk_percent = parseInt(this.disk_gb_used / this.disk_gb_total * 100);
+				this.memory_gb_total = get_gb_rounded(this.node.memory_size);
+				this.memory_gb_used = get_gb_rounded(this.node.memory_used);
+				this.memory_percent = parseInt(this.memory_gb_used / this.memory_gb_total * 100);
+			});
+		},
+		save: function() {
+			update_node(this.node)
+			.then(() => this.update());
 		}
 	},
 	template: /*html*/`
@@ -30,53 +54,97 @@ export default {
 		<template #title>
 			<i class="fa fa-server"></i> Node details
 		</template>
-		<table class="table table-condensed" v-if="node">
-			<tr>
-				<td>ID</td>
-				<td>{{node.id}}</td>
-			</tr>
-			<tr>
-				<td>State</td>
-				<td>{{node.state}}</td>
-			</tr>
-			<tr>
-				<td>Alias</td>
-				<td>{{node.alias}}</td>
-			</tr>
-			<tr>
-				<td>CPU Usage</td>
-				<td>
-					<div class="progress">
-						<div class="progress-bar"
-							v-bind:style="{ width: node.load_percent + '%' }">
-							{{node.load_percent}}%
+		<h4>Details</h4>
+		<table class="table" v-if="node">
+			<tbody>
+				<tr>
+					<td>ID</td>
+					<td>{{node.id}}</td>
+				</tr>
+				<tr>
+					<td>Hostname</td>
+					<td>{{node.name}}.{{info_store.hostingdomain_suffix}}</td>
+				</tr>
+				<tr>
+					<td>IPv4</td>
+					<td>{{node.ipv4}}</td>
+				</tr>
+				<tr>
+					<td>IPv6</td>
+					<td>{{node.ipv6}}</td>
+				</tr>
+				<tr>
+					<td>State</td>
+					<td>{{node.state}}</td>
+				</tr>
+				<tr>
+					<td>Alias</td>
+					<td>
+						<div class="btn-group w-100">
+							<input type="text" class="form-control" v-model="node.alias"/>
+							<button class="btn btn-xs btn-info" v-on:click="save()">
+								<i class="fa fa-floppy-disk"></i>
+							</button>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>CPU Usage</td>
+					<td>
+						<div class="progress">
+							<div class="progress-bar" v-bind:style="{ width: node.load_percent + '%' }">
+								{{node.load_percent}}%
 							</div>
-					</div>
-				</td>
-			</tr>
-			<tr>
-				<td>Disk Usage</td>
-				<td>
-					0/{{disk_gb_total}} GB
-					<div class="progress">
-						<div class="progress-bar bg-warning" style="width: 10%">
-							10%
 						</div>
-					</div>
-				</td>
-			</tr>
-			<tr>
-				<td>Memory Usage</td>
-				<td>
-					0.2/2.0 GB
-					<div class="progress">
-						<div class="progress-bar bg-danger" style="width: 10%">
-							10%
+					</td>
+				</tr>
+				<tr>
+					<td>Disk Usage</td>
+					<td>
+						{{disk_gb_used}}/{{disk_gb_total}} GB
+						<div class="progress">
+							<div class="progress-bar bg-warning" v-bind:style="{ width: disk_percent + '%' }">
+								{{disk_percent}}%
+							</div>
 						</div>
-					</div>
-				</td>
-			</tr>
-
+					</td>
+				</tr>
+				<tr>
+					<td>Memory Usage</td>
+					<td>
+						{{memory_gb_used}}/{{memory_gb_total}} GB
+						<div class="progress">
+							<div class="progress-bar bg-danger" v-bind:style="{ width: memory_percent + '%' }">
+								{{memory_percent}}%
+							</div>
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<h4>Servers</h4>
+		<table class="table">
+			<thead>
+				<th>Name</th>
+				<th>Created</th>
+				<th>State</th>
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+						<a href="todo">
+							<i class="fa fa-list"></i>
+							my-server
+						</a>
+					</td>
+					<td>
+						20xx-yy-zz
+					</td>
+					<td>
+						state
+					</td>
+				</tr>
+			</tbody>
 		</table>
 	</card-layout>
 	`
