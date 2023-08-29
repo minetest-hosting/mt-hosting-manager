76033c815489c5e503232c82068d7986090ed573
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-29 06:31:09 +0200

    restart docker service after first provisioning

--- worker/provision/setup.sh
+++ worker/provision/setup.sh
@@ -8,8 +8,10 @@ test -f "APT_STAGE1" ||{
     apt-get update
     apt-get install -y docker docker-compose net-tools iptables-persistent
     ip6tables-restore /etc/iptables/rules.v6
+    docker network create --ipv6 --subnet "fd00:dead:beef::/48" terminator || true
+    systemctl restart docker
+
     touch "APT_STAGE1"
 }
 
-docker network create --ipv6 --subnet "fd00:dead:beef::/48" terminator || true
 docker-compose up -d
