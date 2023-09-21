32e462e59adb5c27e153c4e071d56e9603215473
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-09-21 20:22:11 +0200

    source button

--- public/js/components/pages/Start.js
+++ public/js/components/pages/Start.js
@@ -15,6 +15,9 @@ export default {
 	<card-layout title="Home" icon="home" :breadcrumb="breadcrumb">
 		<div class="text-center">
 			<h4>Minetest hosting</h4>
+			<a class="btn btn-secondary" href="https://github.com/minetest-hosting/mt-hosting-manager" target="new">
+				<i class="fa-brands fa-github"></i> Source
+			</a>
 			<hr/>
 			<img src="assets/minetest-hosting-600px.png" class="img img-rounded"/>
 		</div>
