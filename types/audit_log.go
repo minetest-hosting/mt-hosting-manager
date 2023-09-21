a7b64f9dc12be1d4665cb56f644e4bd8cdc7d90c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-09-21 15:03:54 +0200

    audit log

---
+++ types/audit_log.go
@@ -0,0 +1,36 @@
+package types
+
+func AuditLogProvider() *AuditLog { return &AuditLog{} }
+
+type AuditLogType string
+
+const (
+	AuditLogUserCreated AuditLogType = "user_created"
+)
+
+type AuditLog struct {
+	ID                   string       `json:"id"`
+	Type                 AuditLogType `json:"type"`
+	Timestamp            int64        `json:"timestamp"`
+	UserID               string       `json:"user_id"`
+	UserNodeID           *string      `json:"user_node_id"`
+	MinetestServerID     *string      `json:"minetest_server_id"`
+	PaymentTransactionID *string      `json:"payment_transaction_id"`
+	Message              string       `json:"message"`
+}
+
+func (m *AuditLog) Columns(action string) []string {
+	return []string{"id", "type", "timestamp", "user_id", "user_node_id", "minetest_server_id", "payment_transaction_id", "message"}
+}
+
+func (m *AuditLog) Table() string {
+	return "audit_log"
+}
+
+func (m *AuditLog) Scan(action string, r func(dest ...any) error) error {
+	return r(&m.ID, &m.Type, &m.Timestamp, &m.UserID, &m.UserNodeID, &m.MinetestServerID, &m.PaymentTransactionID, &m.Message)
+}
+
+func (m *AuditLog) Values(action string) []any {
+	return []any{m.ID, m.Type, m.Timestamp, m.UserID, m.UserNodeID, m.MinetestServerID, m.PaymentTransactionID, m.Message}
+}
