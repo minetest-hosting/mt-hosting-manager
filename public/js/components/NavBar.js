57329bca954c72962079428dfb300f161f6a605d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-09-28 11:15:48 +0200

    jobs link

--- public/js/components/NavBar.js
+++ public/js/components/NavBar.js
@@ -61,7 +61,7 @@ export default {
 							<i class="fa fa-envelope"></i> Send mail
 						</router-link>
 					</li>
-					<li class="nav-item" v-if="is_logged_in">
+					<li class="nav-item" v-if="has_role('ADMIN')">
 						<router-link to="/jobs" class="nav-link">
 							<i class="fa fa-play"></i> Jobs
 						</router-link>
