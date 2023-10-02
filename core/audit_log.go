7bbf64f398759fa2a6b37967d5702b45c4727bfc
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-10-02 06:46:32 +0200

    bump audit log to info level

--- core/audit_log.go
+++ core/audit_log.go
@@ -16,7 +16,7 @@ func (c *Core) AddAuditLog(l *types.AuditLog) {
 		"user_node_id":           l.UserNodeID,
 		"minetest_server_id":     l.MinetestServerID,
 		"payment_transaction_id": l.PaymentTransactionID,
-	}).Debug("audit-log")
+	}).Info("audit-log")
 
 	err := c.repos.AuditLogRepo.Insert(l)
 	if err != nil {
