b6e7cea7b05f00a5632b37f46cfb1689043899c1
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-10-06 13:59:20 +0200

    start page with images

--- public/js/components/pages/Start.js
+++ public/js/components/pages/Start.js
@@ -15,6 +15,8 @@ export default {
 	<card-layout title="Home" icon="home" :breadcrumb="breadcrumb">
 		<div class="text-center">
 			<h4>Minetest hosting</h4>
+			<img src="assets/minetest-hosting-80px.png" class="img img-rounded"/>
+			<hr/>
 			<router-link class="btn btn-secondary" to="/pricing">
 				<i class="fa fa-money-bill"></i> Pricing
 			</router-link>
@@ -43,8 +45,55 @@ export default {
 				<i class="fa-brands fa-discord"></i>
 				Join the discord server
 			</a>
-			<hr/>
-			<img src="assets/minetest-hosting-600px.png" class="img img-rounded"/>
+		</div>
+		<hr/>
+		<div class="text-center">
+			<h4>Features</h4>
+		</div>
+		<div class="row">
+			<div class="col-6">
+				<h5>Easy server provisioning</h5>
+				<ul>
+					<li>Create servers with just a few clicks</li>
+					<li>IPv6 support by default</li>
+					<li>Secured with TLS (the management interface)</li>
+					<li>Free *.minetest.ch domain included or bring own</li>
+				</ul>
+			</div>
+			<div class="col-6">
+				<img src="assets/features_create_server.png" class="img img-rounded"/>
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-6">
+				<h5>Server management web-interface</h5>
+				<ul>
+					<li>Use the wizard to set up your server or change every setting you want</li>
+					<li>Easy mod management with ContentDB and Git support</li>
+					<li>Player- and Ban-Management out of the box</li>
+					<li>Integrations for SkinsDB and ingame Mail available</li>
+					<li>Execute chat-commands or inject lua-code through the web-interface</li>
+					<li>Chat with your players</li>
+				</ul>
+			</div>
+			<div class="col-6">
+				<img src="assets/feature_mod_management.png" class="img img-rounded"/>
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-6">
+				<h5>Wide choice of servers and locations</h5>
+				<ul>
+					<li>Available locations: Germany 🇩🇪 and United States 🇺🇸</li>
+					<li>Select your server according to demand, see <router-link to="/pricing">Pricing</router-link></li>
+					<li>Run multiple minetest-engines on a single server</li>
+				</ul>
+			</div>
+			<div class="col-6">
+				<img src="assets/features_node_types.png" class="img img-rounded"/>
+			</div>
 		</div>
 	</card-layout>
 	`
