3ae8141be10dff735fb3be8d83aec28868ac53e8
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-10-06 08:35:36 +0200

    notification send error log

--- notify/notify.go
+++ notify/notify.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
 	_ "time/tzdata"
+
+	"github.com/sirupsen/logrus"
 )
 
 type NtfyNotification struct {
@@ -22,22 +24,24 @@ var ntfy_topic = os.Getenv("NTFY_TOPIC")
 var ntfy_username = os.Getenv("NTFY_USERNAME")
 var ntfy_password = os.Getenv("NTFY_PASSWORD")
 
-func Send(nn *NtfyNotification, cache bool) error {
+func Send(nn *NtfyNotification, cache bool) {
 	if ntfy_url == "" || ntfy_topic == "" {
 		// nothing to do
-		return nil
+		return
 	}
 
 	nn.Topic = ntfy_topic
 
 	body, err := json.Marshal(nn)
 	if err != nil {
-		return err
+		logrus.WithError(err).Error("failed to marshal notification")
+		return
 	}
 
 	req, err := http.NewRequest(http.MethodPost, ntfy_url, bytes.NewReader(body))
 	if err != nil {
-		return err
+		logrus.WithError(err).Error("failed to create request for notification")
+		return
 	}
 
 	if !cache {
@@ -51,9 +55,8 @@ func Send(nn *NtfyNotification, cache bool) error {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return err
+		logrus.WithError(err).Error("failed to post notification")
+		return
 	}
 	defer resp.Body.Close()
-
-	return nil
 }
