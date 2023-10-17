1631428b8b4ed328f6d89c233e9cbf8137afc560
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-10-17 13:24:57 +0200

    currency table

--- db/migrations/01_init.up.sql
+++ db/migrations/01_init.up.sql
@@ -132,3 +132,10 @@ create table audit_log(
 );
 
 create index audit_log_search on audit_log(type, timestamp, user_id);
+
+create table exchange_rate(
+    currency varchar(16) primary key not null, -- EUR, ETH, BTC
+    display_name varchar not null, -- "United States Dollar"
+    rate varchar(64) not null, -- EUR * rate = $currency
+    digits int not null default 2 -- 0.01
+);
