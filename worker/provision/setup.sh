8730e437bab8d3dc78bfae014616bba0e893e05b
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-09-17 20:40:31 +0200

    increase delay between apt update and install

--- worker/provision/setup.sh
+++ worker/provision/setup.sh
@@ -6,7 +6,7 @@ cd `dirname $0`
 
 test -f "APT_STAGE1" ||{
     apt-get update
-    sleep 2
+    sleep 5
     apt-get install -y docker docker-compose net-tools iptables-persistent
     docker network create --ipv6 --subnet "fd00:dead:beef::/48" terminator || true
     touch "APT_STAGE1"
