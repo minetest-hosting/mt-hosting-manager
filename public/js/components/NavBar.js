72ff9893fe0be92a8b769d9f06d72c414eeb5c5e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-01-04 08:47:01 +0100

    replace title with icon

--- public/js/components/NavBar.js
+++ public/js/components/NavBar.js
@@ -26,7 +26,9 @@ export default {
 	template: /*html*/`
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 			<div class="container-fluid">
-				<router-link to="/" class="navbar-brand">Minetest hosting</router-link>
+				<router-link to="/" class="navbar-brand">
+					<img src="./assets/minetest-hosting-80px.png">
+				</router-link>
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 					<li class="nav-item">
 						<router-link to="/" class="nav-link">
