02e7da8087688a6555692602cac7dbdcfc5367d3
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-01-06 13:24:38 +0100

    exchange rate api

--- public/js/api/exchange_rate.js
+++ public/js/api/exchange_rate.js
@@ -1,3 +1,19 @@
 import { protected_fetch } from "./protected_fetch.js";
 
 export const get_exchange_rates = () => protected_fetch(`api/exchange_rate`);
+
+export const get_exchange_rate = currency => protected_fetch(`api/exchange_rate/${currency}`);
+
+export const create_exchange_rate = er => protected_fetch(`api/exchange_rate`, {
+    method: "POST",
+    body: JSON.stringify(er)
+});
+
+export const update_exchange_rate = er => protected_fetch(`api/exchange_rate/${er.currency}`, {
+    method: "PUT",
+    body: JSON.stringify(er)
+});
+
+export const delete_exchange_rate = currency => protected_fetch(`api/exchange_rate/${currency}`, {
+    method: "DELETE"
+});
